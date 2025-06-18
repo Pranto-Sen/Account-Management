6a1abe298adf0d20202bf56ea35c997c7a4b9b8e
Pranto-Sen <prantosen56@gmail.com>
2025-06-19 00:17:51 +0800

    Database configured with Identity roles initialized

---
+++ Account Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Account_Management.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var adminRole = new IdentityRole
+            {
+                Id = "1", 
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            };
+
+            var accountantRole = new IdentityRole
+            {
+                Id = "2",
+                Name = "Accountant",
+                NormalizedName = "ACCOUNTANT"
+            };
+
+            var viewerRole = new IdentityRole
+            {
+                Id = "3",
+                Name = "Viewer",
+                NormalizedName = "VIEWER"
+            };
+
+            builder.Entity<IdentityRole>().HasData(adminRole, accountantRole, viewerRole);
+        }
+    }
+}
