f175a7ec80e12198111ee2e5335036818ad41c4e
Pranto-Sen <prantosen56@gmail.com>
2025-06-21 00:15:20 +0800

    Add models for Account, Voucher, VoucherEntry, and RolePermission

---
+++ Account Management/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace Account_Management.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public int? ParentAccountId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Account Management/Models/RolePermission.cs
@@ -0,0 +1,11 @@
+﻿namespace Account_Management.Models
+{
+    public class RolePermission
+    {
+        public string RoleId { get; set; }
+        public string ModuleName { get; set; }
+        public bool CanView { get; set; }
+        public bool CanEdit { get; set; }
+        public bool CanDelete { get; set; }
+    }
+}
