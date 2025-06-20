6e19e08411fbf3a4c7ac4306725c13a670329155
Pranto-Sen <prantosen56@gmail.com>
2025-06-21 00:49:25 +0800

    Add Excel export feature to ChartOfAccounts index page

--- Account Management/Pages/ChartOfAccounts/Index.cshtml.cs
+++ Account Management/Pages/ChartOfAccounts/Index.cshtml.cs
@@ -1,9 +1,13 @@
 using Account_Management.Models;
 using Account_Management.Services;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Account_Management.Pages.ChartOfAccounts
 {
+    [Authorize(Roles = "Admin,Accountant")]
     public class IndexModel : PageModel
     {
         private readonly DatabaseService _databaseService;
@@ -39,6 +43,35 @@ namespace Account_Management.Pages.ChartOfAccounts
                 .ToList();
         }
 
-       
+        public IActionResult OnPostExport()
+        {
+            var accounts = _databaseService.GetChartOfAccounts();
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("ChartOfAccounts");
+                worksheet.Cell(1, 1).Value = "Account ID";
+                worksheet.Cell(1, 2).Value = "Account Name";
+                worksheet.Cell(1, 3).Value = "Account Type";
+                worksheet.Cell(1, 4).Value = "Parent Account ID";
+                worksheet.Cell(1, 5).Value = "Is Active";
+
+                int row = 2;
+                foreach (var account in accounts)
+                {
+                    worksheet.Cell(row, 1).Value = account.AccountId;
+                    worksheet.Cell(row, 2).Value = account.AccountName;
+                    worksheet.Cell(row, 3).Value = account.AccountType;
+                    worksheet.Cell(row, 4).Value = account.ParentAccountId?.ToString() ?? "";
+                    worksheet.Cell(row, 5).Value = account.IsActive;
+                    row++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ChartOfAccounts.xlsx");
+                }
+            }
+        }
     }
 }
